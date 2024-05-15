8b3fdcec372465b751d35b7264612ed8b2b71602
kame-sama <ad.bodeev@gmail.com>
2024-05-15 11:47:54 +0500

    add loader

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,15 +1,24 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/Theme';
 import { GlobalStyles } from '../styles/Global';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData } from 'react-router-dom';
 import Header from '../components/Header';
 
+export async function loader() {
+  const url = 'https://fakestoreapi.com/products/categories';
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`Error: ${response.status}`);
+  const categories = await response.json();
+  return categories as string[];
+}
+
 export default function Root() {
+  const categories = useLoaderData() as string[];
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Header />
-      <Outlet />
+      <Outlet context={{ categories }} />
     </ThemeProvider>
   );
 }
