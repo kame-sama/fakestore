c7b251dea7ff0210fdb5bb1bef9128bbe7fb04fe
kame-sama <ad.bodeev@gmail.com>
2024-05-15 09:44:46 +0500

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Container } from './Container';
+
+const HeaderContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  gap: 1rem;
+`;
+
+const StyledLink = styled(NavLink)`
+  color: ${({ theme }) => theme.colors.slate[950]};
+  font-size: 1.1rem;
+`;
+
+const Logo = styled(StyledLink)`
+  font-size: 2rem;
+  text-decoration: none;
+`;
+
+const Accent = styled.span`
+  font-weight: 900;
+`;
+
+export default function Header() {
+  return (
+    <header>
+      <HeaderContainer>
+        <Logo to="/">
+          FAKEst<Accent>o</Accent>re
+        </Logo>
+        <NavBar>
+          <StyledLink to="shop">Shop</StyledLink>
+          <StyledLink to="bag">Bag(0)</StyledLink>
+        </NavBar>
+      </HeaderContainer>
+    </header>
+  );
+}
