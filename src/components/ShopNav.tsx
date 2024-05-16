e875b718440af0ff87e2264020c9e50d1c76f57f
kame-sama <ad.bodeev@gmail.com>
2024-05-16 09:03:22 +0500

    add accent on active and pending links

--- src/components/ShopNav.tsx
+++ src/components/ShopNav.tsx
@@ -2,7 +2,17 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledLink = styled(NavLink)`
-  color: ${({ theme }) => theme.colors.slate[950]};
+  color: ${({ theme }) => theme.colors.slate[700]};
+  font-weight: 300;
+
+  &.active {
+    font-weight: 600;
+  }
+
+  &.pending {
+    color: ${({ theme }) => theme.colors.slate[900]};
+    font-weight: 400;
+  }
 `;
 
 const NavBar = styled.nav`
@@ -15,7 +25,7 @@ export default function ShopNav({ categories }: { categories: string[] }) {
     <NavBar>
       <ul>
         <li>
-          <StyledLink to="/shop">all</StyledLink>
+          <StyledLink to="/shop/all">all</StyledLink>
         </li>
         {categories.map((category) => (
           <li key={category}>
