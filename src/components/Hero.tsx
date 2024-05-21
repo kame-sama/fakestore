ba4ddbadd47500bb93353f36847adbf39f71965e
kame-sama <ad.bodeev@gmail.com>
2024-05-21 09:28:07 +0500

    add webm to fix video size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
-import hero from '../assets/hero.mp4';
+import hero from '../assets/hero.webm';
+import heroFallback from '../assets/hero.mp4';
 
 const HeroContainer = styled.div`
   width: 100vw;
@@ -26,9 +27,13 @@ const HeroButton = styled(Button)`
 `;
 
 const Video = styled.video`
-  position: fixed;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   z-index: -1;
-  object-fit: contain;
+  object-fit: cover;
   object-position: center;
 `;
 
@@ -36,7 +41,8 @@ export default function Hero() {
   return (
     <HeroContainer>
       <Video autoPlay loop muted playsInline>
-        <source src={hero} type="video/mp4" />
+        <source src={hero} type="video/webm" />
+        <source src={heroFallback} type="video/mp4" />
       </Video>
       <HeroTitle>Welcome to the FAKEstore</HeroTitle>
       <HeroButton as={Link} to="shop">
